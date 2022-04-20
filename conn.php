c0f0f42c86987e409e5de4bf2402c93346f83c5a
Rafael Rivero <rafalejandrorivero@gmail.com>
2022-04-19 22:07:31 -0400

    Update

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'enlacecc_ventas');
+	$conn = new mysqli('localhost', 'root', '', 'enlace cc');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
